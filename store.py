53108d7771eaa007a22ec1fab76d2b311575400b
HsuShengHsien <t0987235673@gmail.com>
2018-12-19 23:12:51 +0800

    Add files via upload

---
+++ store.py
@@ -0,0 +1,237 @@
+class Store:
+
+    #'': '店名: \n地址: \n營業時段: \n公休: \n電話: ',
+    #'': {'disc': '','img': ''},
+
+# noodles west
+    info_noodles_store_in_west = {
+        '桃花源牛肉麵': '店名: 桃花源牛肉麵\n地址: 台南市中西區南寧街130號\n營業時段: 11:00-14:30, 17:00-21:00\n公休: 週三\n電話: 06-2130670',
+        '恭仔肉燥意麵': '店名: 恭仔肉燥意麵\n地址: 台南市中西區新美街32號\n營業時段: 11:00-23:00\n公休: 無\n電話: 06-2217506',
+        '醇涎坊鍋燒意麵': '店名: 醇涎坊鍋燒意麵\n地址: 台南市中西區保安路53號\n營業時段: 06:00-22:00\n公休: 無\n電話: 06-2215003',
+        '小丰川': '店名: 小丰川\n地址: 台南市中西區國華街三段261號\n營業時段: 12:00-20:00\n公休: 週 日.一\n電話: 無',
+    }
+
+    menu_noodles_store_in_west = {
+        '桃花源牛肉麵': 'https://i.imgur.com/849vHSx.jpg',
+        '恭仔肉燥意麵': 'https://i.imgur.com/h8ETjuT.jpg',
+        '醇涎坊鍋燒意麵': 'https://i.imgur.com/NGl01N5.jpg',
+        '小丰川': 'https://i.imgur.com/x5PJwbA.jpg',
+    }
+
+    recommmend_noodles_store_in_west = {
+        '桃花源牛肉麵': {
+            'disc': '牛肉麵的湯頭溫醇，除了牛肉的香氣外，還有蔬菜的淡雅',
+            'img': 'https://i.imgur.com/YeCTINP.jpg'},
+        '恭仔肉燥意麵': {
+            'disc': '台南有名的意麵店之一，意麵的特點在於有加蒜泥，口味辛香，此外水餃也非常推薦，尤其是蝦餃，包了2隻蝦呢！',
+            'img': 'https://i.imgur.com/2EPNNIp.jpg'}, 
+        '醇涎坊鍋燒意麵': {
+            'disc': '台南鍋燒意麵百百家中有名的一家，湯頭帶溫厚的油炸物香，但是又不會太油膩，炸魚餅很好吃',
+            'img': 'https://i.imgur.com/PRJXmBm.jpg'},
+        '小丰川': {
+            'disc': '有種懷舊感的麵店，麵條口感有厚度有彈性，加肉燥的麻醬麵很好吃',
+            'img': 'https://i.imgur.com/pHFt7gE.jpg'},
+    }
+
+# rice west
+    info_rice_store_in_west = {
+        'OMHUT': '店名: OMHUT\n地址: 台南市中西區萬昌街79號\n營業時段: 11:30-15:00, 17:30-20:00\n公休: 週日\n電話: 06-2215008',
+        '矮仔成': '店名: 矮仔成蝦仁飯\n地址: 台南市中西區海安路一段66號\n營業時段: 8:30-19:30\n公休: 週二\n電話: 06-2201897',
+        '老泰羊肉': '店名: 老泰羊肉\n地址: 台南市中西區青年路206號\n營業時段: 17:00-01:00\n公休: 無\n電話: 06-2262344',
+        '橫町丼屋': '店名: 橫町丼屋\n地址: 台南市中西區樹林街二段16號\n營業時段: 11:30-14:00, 17:00-21:00\n公休: 週一\n電話: 06-2141908',
+    }
+
+    menu_rice_store_in_west = {
+        'OMHUT': 'https://i.imgur.com/s2kL59v.jpg',
+        '矮仔成': 'https://i.imgur.com/v8jBQh2.jpg',
+        '老泰羊肉': 'https://i.imgur.com/xigIAUy.jpg',
+        '橫町丼屋': 'https://i.imgur.com/M58FSJ1.jpg',
+    }
+
+    recommmend_rice_store_in_west = {
+        'OMHUT': {
+            'disc': '人氣很高的店，日式口味咖哩雞很不錯吃，問吧是每天的變化性餐點，建議外帶為主，可於開店前半小時致電訂餐',
+            'img': 'https://i.imgur.com/sivcw5y.jpg'},
+        '矮仔成': {
+            'disc': '台南特色美食蝦仁飯，偏濕潤的口感，火燒蝦的香氣非常撩人，缺點是價位偏高',
+            'img': 'https://i.imgur.com/RrVysNV.jpg'},
+        '老泰羊肉': {
+            'disc': '好吃不帶騷味的羊肉，不需要加大量的油和薑去蓋過騷味，特別推薦羊肉炒飯',
+            'img': 'https://i.imgur.com/8YE7dlR.jpg'},
+        '橫町丼屋': {
+            'disc': '相對平價的日式丼飯，精緻好吃，份量上以男性而言可能稍嫌不足',
+            'img': 'https://i.imgur.com/F0H3V90.jpg'},
+    }
+
+# snack west
+    info_snack_store_in_west = {
+        '康宇家': '店名: 康宇家手作抹茶甜點\n地址: 台南市中西區國華街三段26號D1\n營業時段: 11:00-21:00\n公休: 週四\n電話: 0927-399886',
+        '甜在心': '店名: 元紅豆餅\n地址: 台南市中西區南華街81號\n營業時段: 11:00-17:00\n公休: 週一\n電話: 0921-023020',
+        '冰鄉': '店名: 冰鄉\n地址: 台南市中西區民生路一段160號\n營業時段: 12:00-21:00\n公休: 無\n電話: 06-2234427',
+        '幸の町': '店名: 幸の町 雪花冰專賣店\n地址: 台南市中西區海安路二段48號\n營業時段: 13:30-22:00\n公休: 無\n電話: 06-2227185',
+    }
+
+    menu_snack_store_in_west = {
+        '康宇家': 'https://i.imgur.com/ndjYNOF.jpg',
+        '甜在心': 'https://i.imgur.com/jz2ilka.jpg',
+        '冰鄉': 'https://i.imgur.com/4kAqbhO.jpg',
+        '幸の町': 'https://i.imgur.com/hFulis8.jpg',
+    }
+
+    recommmend_snack_store_in_west = {
+        '康宇家': {
+            'disc': '主打茶系列的霜淇淋，抹茶味不會很重，怕抹茶的人也能接受的味道',
+            'img': 'https://i.imgur.com/o7Oj48x.jpg'},
+        '甜在心': {
+            'disc': '好吃又美拍的車輪餅，薄脆的外皮，滿滿的內餡，奶油經典口味，花生帶顆粒也很不錯，店家時不時會推出特殊口味',
+            'img': 'https://i.imgur.com/udzCl40.jpg'},
+        '冰鄉': {
+            'disc': '台南出名的冰店，給料大方，尤其夏季芒果冰，秋季荔枝冰，冬季草莓冰，除了觀光客，也緊抓不少在地人的心',
+            'img': 'https://i.imgur.com/9L8IIrp.jpg'},
+        '幸の町': {
+            'disc': '漂亮日式感的冰店，冰品走精緻路線很好拍照，不過口味偏甜',
+            'img': 'https://i.imgur.com/MsvVJsL.jpg'},
+    }
+
+# noodles north
+    info_noodles_store_in_north = {
+        '乖乖牛肉麵': '店名: 乖乖牛肉麵\n地址: 台南市北區北門路三段23號\n營業時段: 11:30-13:30, 17:00-20:00\n公休: 無\n電話: 0908-686135',
+        '明月麵店': '店名: 明月麵店\n地址: 台南市北區成功路68巷4號\n營業時段: 10:00-20:00\n公休: 無\n電話: 06-2278759',
+    }
+
+    menu_noodles_store_in_north = {
+        '乖乖牛肉麵': 'https://i.imgur.com/qi1Vo6k.jpg',
+        '明月麵店': 'https://i.imgur.com/w7a1uK6.jpg',
+    }
+
+    recommmend_noodles_store_in_north = {
+        '乖乖牛肉麵': {
+            'disc': '高CP值的牛肉麵，麵多肉多又好吃，大食量的人一定可以獲得飽足感',
+            'img': 'https://i.imgur.com/oSOSsch.jpg'},
+        '明月麵店': {
+            'disc': '特別隱匿的麵攤，麻醬麵加肉燥香而不油膩，不喜歡麻醬的人可以不要加',
+            'img': 'https://i.imgur.com/SmQV18H.jpg'},
+    }
+
+# rice north
+    info_rice_store_in_north = {
+        '不餓門廣東粥': '店名: 不餓門廣東粥\n地址: 台南市北區文賢路162號\n營業時段: 11:00-14:00, 17:00-20:00\n公休: 週三\n電話: 06-3585468',
+        '開元路無名虱目魚': '店名: 開元路無名虱目魚 ‧ 肉燥飯\n地址: 台南市北區開元路313號\n營業時段: 05:20-13:30\n公休: 無\n電話: 06-2363646',
+    }
+
+    menu_rice_store_in_north = {
+        '不餓門廣東粥': 'https://i.imgur.com/0KYJff1.jpg',
+        '開元路無名虱目魚': 'https://i.imgur.com/6AfVis4.jpg',
+    }
+
+    recommmend_rice_store_in_north = {
+        '不餓門廣東粥': {
+            'disc': '粥品很有味道，是重口味的粥，比粥更令人印象深刻的是炒飯，尤其蝦仁炒飯，非常推薦',
+            'img': 'https://i.imgur.com/ArOhPSi.jpg'},
+        '開元路無名虱目魚': {
+            'disc': '台南在地人的早餐，肉燥飯肥嫩入口即化，魚皮湯很特別是魚漿製成',
+            'img': 'https://i.imgur.com/PAXCa2k.jpg'},
+
+    }
+
+# snack north
+    info_snack_store_in_north = {
+        '冰仔角': '店名: 冰仔角\n地址: 台南市北區北園街87巷34號\n營業時段: 13:00-19:00\n公休: 週日\n電話: 06-2380699',
+        '阿堯師': '店名: 老正牌 • 阿堯師雞蛋糕\n地址: 台南市北區文賢路522號\n營業時段: 13:00-22:00\n(週日: 14:30-20:30)\n公休: 無\n電話: 06-3500820',
+    }
+
+    menu_snack_store_in_north = {
+        '冰仔角': 'https://i.imgur.com/NZENM8P.jpg',
+        '阿堯師': 'https://i.imgur.com/USbNKb0.jpg'
+    }
+
+    recommmend_snack_store_in_north = {
+        '冰仔角': {
+            'disc': '隱匿但人潮很多的冰店，冰品高CP值，招牌番茄蜜餞牛奶冰非常好吃',
+            'img': 'https://i.imgur.com/tqrR48u.jpg'},
+        '阿堯師': {
+            'disc': '在地台南人一定不陌生的小點心，雞蛋糕帶蜂蜜香氣，口感鬆軟，非常推薦！',
+            'img': 'https://i.imgur.com/rqWZBtD.jpg'},
+    }
+
+# rice east
+    info_rice_store_in_east = {
+        '浜家咖喱屋': '店名: 浜家咖喱屋\n地址: 台南市東區東安路48巷29號\n營業時段: 11:00-14:00, 17:00-20:30\n公休: 週一\n電話: 06-2008556',
+        '鴨霸當歸鴨-東安店': '店名: 鴨霸當歸鴨-東安店\n地址: 台南市東區東安路1號\n營業時段: 10:00-22:00\n公休: 週日\n電話: 06-2094977',
+        '唐家泡菜館': '店名: 唐家泡菜館\n地址: 台南市東區裕農路18號\n營業時段: 11:00-14:00, 16:30-20:00\n公休: 週日\n電話: 06-2763263',
+        '兒玉家和食串炸': '店名: 兒玉家和食串炸\n地址: 台南市東區東寧路134巷4-3號\n營業時段: 17:30-21:30\n公休: 週 一.二.三\n電話: 0966-047800'
+    }
+
+    menu_rice_store_in_east = {
+        '浜家咖喱屋': "https://i.imgur.com/2FkGu7g.jpg",
+        '鴨霸當歸鴨-東安店': "https://i.imgur.com/twHJVJU.jpg",
+        '唐家泡菜館': "https://i.imgur.com/uEdAyvA.jpg",
+        '兒玉家和食串炸': "https://i.imgur.com/orO5fZQ.jpg"
+    }
+
+    recommmend_rice_store_in_east = {
+        '浜家咖喱屋': {
+            'disc': '主打溫潤好吃的日式咖哩，另外咖哩提供辣度調整，符合嗜辣的人',
+            'img': 'https://i.imgur.com/Ta2kv6D.jpg'},
+        '鴨霸當歸鴨-東安店': {
+            'disc': '藥感不重的當歸鴨很香，飯系列口味夠重，鴨肉飯去骨體貼懶人',
+            'img': 'https://i.imgur.com/hFef8ZX.jpg'
+        },
+        '唐家泡菜館': {
+            'disc': '餐點以微酸微辣的泡菜為特色，尤其是「泡菜三鮮炒飯」評價最高，用餐時段候餐很久，建議事先訂餐',
+            'img': 'https://i.imgur.com/kkesXAC.jpg'
+        },
+        '兒玉家和食串炸': {
+            'disc': '高CP值又好吃的日式定食，肉給得很多，大份量一般男性也會很飽足，特別推薦炸牛排',
+            'img': 'https://i.imgur.com/H0uCUI3.jpg'
+        }
+    }
+# noodles east
+    info_noodles_store_in_east = {
+        '江西風味外省麵': '店名: 江西風味外省麵\n地址: 台南市東區青年路257號\n營業時段: 11:00-14:00, 17:00-20:00\n公休: 週三\n電話: 06-2080462',
+        '川采精緻麵食館': '店名: 川采精緻麵食館\n地址: 台南市東區富農街二段77號\n營業時段: 10:30-14:00, 17:00-21:00\n公休: 週三\n電話: 06-2897567',
+        '穀秣麵食屋': '店名: 穀秣麵食屋\n地址: 台南市東區裕農路350巷9號\n營業時段: 11:00-14:00, 17:00-20:30\n公休: 週三\n電話: 06-2087276',
+        '忠味麵館': '店名: 忠味麵館\n地址: 台南市東區裕忠路87號\n營業時段: 7:00-14:30\n公休: 週 日.一\n電話: 06-3315089',
+        '垵口麵攤': '店名: 垵口麵攤\n地址: 台南市東區富農街一段290-1號\n營業時段: 11:00-14:00, 17:00-21:00\n公休: 無\n電話: 0939-632516'
+    }
+
+    menu_noodles_store_in_east = {
+        '江西風味外省麵': "https://i.imgur.com/9NJPYIy.jpg",
+        '川采精緻麵食館': "https://i.imgur.com/z4XXIve.jpg",
+        '穀秣麵食屋': "https://i.imgur.com/rGvdM18.jpg",
+        '忠味麵館': "https://i.imgur.com/J4VpxHP.jpg",
+        '垵口麵攤': "https://i.imgur.com/uNWvwXY.jpg"
+    }
+
+    recommmend_noodles_store_in_east = {
+        '垵口麵攤': {
+            'disc': '主打雞蛋麵QQ的口感，喜好咀嚼的麵控一定會喜歡的，口味清淡，不過份量偏少',
+            'img': 'https://i.imgur.com/ASVM0yT.jpg'},
+        '江西風味外省麵': {
+            'disc':'手作的外省麵，麵的厚度偏薄，不嗜辣推薦「蒜香乾麵」，嗜辣的話推薦「香辣拌麵」',
+            'img': 'https://i.imgur.com/Htem2d2.jpg'},
+        '忠味麵館': {
+            'disc': '主打榨菜肉絲麵，口味較重，微微辣，適合早餐想吃重口味中式麵食的時候',
+            'img': 'https://i.imgur.com/VRs869u.jpg'},
+        '川采精緻麵食館': {
+            'disc': '川味麵食，最推薦「榨菜肉絲麵」，湯頭清甜好喝，榨菜和肉絲給的量也很多，吃著很滿足',
+            'img': 'https://i.imgur.com/np5NfOA.jpg'},
+        '穀秣麵食屋': {
+            'disc': '滷味特別亮眼的一家麵館，尤其是三色蛋，常常完售一空',
+            'img': 'https://i.imgur.com/YlhdtD7.jpg'}
+    }
+
+# snack east
+    info_snack_store_in_east = {
+        '木子家八寶冰': '店名: 木子家八寶冰\n地址: 台南市東區大同路一段187號\n營業時段: 11:00-22:00\n公休: 無\n電話: 06-2159068'
+    }
+
+    menu_snack_store_in_east = {
+        '木子家八寶冰': 'https://i.imgur.com/BTVXipJ.jpg',
+    }
+
+    recommmend_snack_store_in_east = {
+        '木子家八寶冰': {
+            'disc': '八寶冰淋上黑糖水，夏天吃很過癮，冬天供應的草莓牛奶冰也很平價好吃呢',
+            'img': 'https://i.imgur.com/pLJnK8V.jpg'},
+    }
